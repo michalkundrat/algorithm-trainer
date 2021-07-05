af21196631acb3b306d95c3c99c8f2392dde2683
ihavezerohealth <michallahcim01@gmail.com>
2021-07-05 19:58:19 +0100

    Tried making a very basic timer. Brutally failed. Then I realised there's a library for that.
    Timer page is about 30% done in its current state.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 import '2Look/2LookOLL.dart';
 import '2Look/2LookPLL.dart';
+import 'timer.dart';
 
 void main() => runApp(MaterialApp(home: AppHomePage()));
 
@@ -35,74 +36,87 @@ class AppHomePage extends StatelessWidget {
                 );
               },
             ),
+            ListTile(
+              title: Text("Timer"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new AppBasicTimer()),
+                  );
+              },
+            ),
           ],
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 15.0,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      Image(image: AssetImage("assets/2-Look/OLL/antisune.png")),
-                      Text("OLL Algorithms"),
-                      SizedBox(height: 7.0,),
-                    ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 15.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    color: Colors.blue,
+                    child: Column(
+                      children: [
+                        Image(image: AssetImage("assets/2-Look/OLL/antisune.png")),
+                        Text("OLL Algorithms"),
+                        SizedBox(height: 7.0,),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      Image(image: AssetImage("assets/2-Look/PLL/Adjacent_Corner_Swap.png")),
-                      Text("PLL Algorithms"),
-                      SizedBox(height: 7.0,),
-                    ],
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    color: Colors.blue,
+                    child: Column(
+                      children: [
+                        Image(image: AssetImage("assets/2-Look/PLL/Adjacent_Corner_Swap.png")),
+                        Text("PLL Algorithms"),
+                        SizedBox(height: 7.0,),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 25.0,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new App2LookAlgorithms()),
-                  );
-                },
-                child: Container(
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Image(image: AssetImage("assets/2-Look/OLL/antisune.png")),
-                          Image(image: AssetImage("assets/2-Look/PLL/H_Perm.png")),
-                        ],
-                      ),
-                      Text("2-Look Algorithms"),
-                      SizedBox(height: 7.0,),
-                    ],
+              ],
+            ),
+            SizedBox(height: 25.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new App2LookAlgorithms()),
+                    );
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image(image: AssetImage("assets/2-Look/OLL/antisune.png")),
+                            Image(image: AssetImage("assets/2-Look/PLL/H_Perm.png")),
+                          ],
+                        ),
+                        Text("2-Look Algorithms"),
+                        SizedBox(height: 7.0,),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          )
-        ],
+              ],
+            ),
+            SizedBox(height: 25.0,)
+          ],
+        ),
       ),
     );
   }
