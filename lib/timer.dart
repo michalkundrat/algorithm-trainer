43e345c1776b530f11b6bc5b7971a49dd43a05a3
ihavezerohealth <michallahcim01@gmail.com>
2021-07-19 14:29:46 +0100

    Added 4x4 scrambling.

--- lib/timer.dart
+++ lib/timer.dart
@@ -11,6 +11,7 @@ late Random random = new Random();
 String lastScramble = "";
 
 String ms_string = "000";
+var possibleMoves4x4 = [possibleMoves3x3, "Fw", "Bw", "Rw", "Lw", "Dw", "Uw", "f", "b", "u", "d", "r", "l"];
 var possibleMoves3x3 = ["R","R'","R2","L","L'","L2","U","U'","U2","D","D'","D2","F","F'","F2","B","B'","B2"];
 var possibleMoves2x2 = ["R","R'","R2","L","L'","L2","U","U'","U2","D","D'","D2","F","F'","F2","B","B'","B2"];
 var possibleMovesPyraminx = ["R","R'","R2","L","L'","L2","U","U'","U2","B","B'","B2","l","l'","r","r'","u","u'","b","b'"];
@@ -18,7 +19,10 @@ var scrambleToDo = "3x3";
 bool readyToScramble = true;
 
 Widget handleScrambleMaking() {
-  if (scrambleToDo == "3x3") {
+  if (scrambleToDo == "4x4") {
+    readyToScramble = false;
+    return Text(generateScramble4x4());
+  } else if (scrambleToDo == "3x3") {
     readyToScramble = false;
     return Text(generateScramble3x3());
   } else if (scrambleToDo == "2x2") {
@@ -32,6 +36,17 @@ Widget handleScrambleMaking() {
   }
 }
 
+String generateScramble4x4() {
+  var lengthOfScramble = random.nextInt(7) + 18;
+  var scrambleAsList = [];
+  for (int iteration = 0; iteration <= lengthOfScramble; iteration++) {
+    var index = random.nextInt(possibleMoves4x4.length);
+    scrambleAsList.add(possibleMoves3x3[index]);
+  }
+  String scramble = scrambleAsList.join(" ");
+  lastScramble = scramble;
+  return scramble;
+}
 String generateScramble3x3() {
   var lengthOfScramble = random.nextInt(7) + 18;
   var scrambleAsList = [];
@@ -172,7 +187,7 @@ class _AppBasicTimerState extends State<AppBasicTimer> {
                         });
                       },
                       value: scrambleToDo,
-                      items: <String>['2x2', '3x3', 'Pyraminx'].map<DropdownMenuItem<String>>((String value) {
+                      items: <String>['2x2', '3x3', '4x4', 'Pyraminx'].map<DropdownMenuItem<String>>((String value) {
                         return DropdownMenuItem<String>(
                           value: value,
                           child: Text(value),
